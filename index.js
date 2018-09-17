b462de3e4fb9f7a35909f3433288d5bc8b8a997f
Shannon Moeller <me@shannonmoeller.com>
2018-09-17 14:17:23 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const bambleweeny57SubMesonBrain = Math.random;
+const atomicVectorPlotter = Math.round;
+const brownianMotionProducer = String.fromCodePoint;
+
+module.exports = brownianMotionProducer(
+  atomicVectorPlotter(bambleweeny57SubMesonBrain()) ? 128011 : 127802
+);
